84151ffcd10ee239f3202f686b812d5baa40e818
smhrd4 <smhrd4@121.147.185.169>
2024-02-07 10:42:17 +0900

    0207-1042-jsm

---
+++ LuxuryClothing/src/main/java/com/main/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.main.controller;
+
+public class MainController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("안녕하세요 !");
+		System.out.println("2024 02 07 제작한 프로젝트입니다.");
+		System.out.println("모두 잘 부탁해요!");
+	}
+
+}
