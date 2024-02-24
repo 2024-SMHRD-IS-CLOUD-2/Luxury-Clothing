43db56d5ea3b9953270fbfc6153878ae669f6fce
smhrd4 <smhrd4@121.147.185.169>
2024-02-24 21:13:31 +0900

    sm 2113

--- LuxuryClothing/src/main/java/com/smhrd/main/controller/YongImgCon.java
+++ LuxuryClothing/src/main/java/com/smhrd/main/controller/YongImgCon.java
@@ -22,19 +22,22 @@ public class YongImgCon implements Controller {
 		
 		// "python" : 그대로 둘 것.
 		// 본인 환경에 맞춰 pydev 프로젝트 경로/파일명.py 로 변경
-		String phdev = "C:\\Users\\smhrd4\\Desktop\\ServerStudy\\PyOCR\\ImagesReder.py";
+		String phdev = "C:/Users/smhrd4/Desktop/ServerStudy/PyOCR/ImagesReder.py";
+//		String phdev = "D:/ImagesReder.py";
+		System.out.println(phdev);
 		
-        ProcessBuilder pb = new ProcessBuilder("PYTHON", phdev);
-        Process p = pb.start();
-        
-        
-        BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+	    // ProcessBuilder를 사용하여 .py 파일 실행
+	    ProcessBuilder processBuilder = new ProcessBuilder("python", phdev).inheritIO();
+	    Process process = processBuilder.start();
 
-		// pydev 모든기능 완료 후 콘솔로 결과 출력
-		while ((line = in.readLine()) != null) {
-			// 실행 결과 처리
-		    System.out.println(line);
-		}
+	    // 실행 결과를 읽어옴
+	    InputStream inputStream = process.getInputStream();
+	    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "EUC-KR"));
+	    StringBuilder output = new StringBuilder();
+	    
+	    while ((line = reader.readLine()) != null) {
+	        output.append(line).append("\n");
+	    }
 		return line;
 	}
 
